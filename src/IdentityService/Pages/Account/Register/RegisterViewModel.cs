2e140b5207ccef8b06c006de4f2681ea2f40a15b
nmartin867 <nick@nmartindev.com>
2024-09-30 03:33:53 -0600

    Adding IdentityService project. Registration page

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Pages.Account.Register;
+
+public class RegisterViewModel
+{
+    [Required, EmailAddress]
+    public string? Email { get; set; }
+    
+    [Required]
+    public string? Username { get; set; }
+    
+    [Required]
+    public string? FullName { get; set; }
+    
+    [Required]
+    public string? Password { get; set; }
+    
+    public string? ReturnUrl { get; set; }
+    
+    public string? Button { get; set; }
+    
+}
